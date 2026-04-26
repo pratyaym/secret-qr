23c37276ecf80d4b0bcde3a50723e9ec29c10344
krindelon <8691511+krindelon@users.noreply.github.com>
2026-04-25 21:35:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
 const CACHE = 'v1';
 
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+
 self.addEventListener('fetch', e =>
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).then(res => {
